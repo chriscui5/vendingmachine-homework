53d558b90938b2121dc747ef57122ac06d4d2ad2
chriscui <chriscui1999@outlook.com>
2019-12-23 15:51:33 +0800

    vending machine vm.welcome() change products.txt

--- src/vending/machine/client/operations.java
+++ src/vending/machine/client/operations.java
@@ -6,10 +6,6 @@ public class operations {
         VendingMachineBuilder vmb = new VendingMachineBuilder();
         VendingMachine vm = vmb.buildVendingMachine();
         vm.welcome();
-        //if you are interested in vm.welcome(),you can try it.It is better than vm.welcome2().
-        //In vm.welcome(),you can understand instruction according to my remainder.
-        //Don't be afraid to make mistakes,my procedure can handle it and give you remainder.
-        //Maybe you think my code is too long,because i complish two functions.
-        vm.welcome2();
+
     }
 }
